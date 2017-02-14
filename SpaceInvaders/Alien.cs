0b87123f762cefd6e7da6222b9445d4dce7f0fbf
RektInator <mlmmlangeveld@gmail.com>
2017-02-14 11:37:02 +0100

    SpaceInvaders classes

---
+++ SpaceInvaders/Alien.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceInvaders
+{
+    class Alien
+    {
+        private Position        _pos;
+        private ConsoleColor    _color;
+
+        public Alien(Position pos, ConsoleColor color)
+        {
+            _pos = pos;
+            _color = color;
+        }
+    }
+}
---
+++ SpaceInvaders/Position.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceInvaders
+{
+    class Position
+    {
+        private int _x, _y;
+
+        public Position(int x, int y)
+        {
+            // set position variables
+            _x = x;
+            _y = y;
+        }
+    }
+}
